e5bc3e13e74555d19131919c788355f27e3e1871
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-08-03 11:29:39 +0200

    Updates testing environment config.

--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,17 @@ Bundler.require(*Rails.groups)
 module BerlinerChoere
   class Application < Rails::Application
 
-    g.test_framework :rspec,
-      :fixtures => true,
-      :view_specs => false,
-      :helper_specs => false,
-      :stylesheets => false,
-      :javascripts => false,
-      :helper => false,
-      :routing_specs => false,
-      :controller_specs => true,
-      :request_specs => true
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :stylesheets => false,
+        :javascripts => false,
+        :helper => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
 
