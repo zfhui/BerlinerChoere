035e111ff297f4ffcdd93e9a0c636e74440b2584
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-10-25 01:06:29 +0200

    Moves herokuapp redirect urls into env vars

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   mount Lockup::Engine, at: '/lockup'
 
+  if ENV['HEROKUAPP_URL'].present? && ENV['PUBLIC_URL'].present?
+    constraints(host: /#{ENV['HEROKUAPP_URL']}/) do
+      match "/(*path)", to: redirect { |params, req| "//#{ENV['PUBLIC_URL']}/#{params[:path]}" }, via: [:get, :post]
+    end
+  end
+  
   root to: "home#index"
 
   devise_for :users,
