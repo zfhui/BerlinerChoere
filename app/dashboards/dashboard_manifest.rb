609df0e580f14cc0abc0998ff9de7bc0300f8503
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2016-02-13 19:29:39 +0100

    Change resource listing at sidebar of administrate

--- app/dashboards/dashboard_manifest.rb
+++ app/dashboards/dashboard_manifest.rb
@@ -10,9 +10,9 @@ class DashboardManifest
   # Dashboards returned from this method must be Rails models for Administrate
   # to work correctly.
   DASHBOARDS = [
-    :users,
+    :choirs,
     :categories,
-    :choirs
+    :users
   ]
 
   # `ROOT_DASHBOARD`
